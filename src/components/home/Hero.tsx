3712d7601353ef764d0c8f905fa594a9453c74a1
Hasanul-Banna <hbdu006@gmail.com>
2022-08-21 12:07:39 +0600

    hero section

---
+++ src/components/home/Hero.tsx
@@ -0,0 +1,62 @@
+import { FC } from "react";
+// import required modules
+import { EffectFade, Autoplay } from "swiper";
+import Image from "next/image";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import img from "../../Images/landscape02.jpg";
+import img2 from "../../Images/landscape03.jpg";
+import img3 from "../../Images/landscape04.jpg";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+
+const Hero: FC = () => {
+  return (
+    <div className="w-full min-h-[100vh]">
+      <Swiper
+        spaceBetween={30}
+        effect={"fade"}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay, EffectFade]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className={"heroWrapper"}>
+            <div className={"imageWrapper"}>
+              <Image
+                priority
+                src={img}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                alt="hero image"
+              />
+            </div>
+
+            <div className="bg-[#00000030] text-white relative flex flex-col justify-center self-center h-[100vh] w-full">
+              <h1 className="mb-4 text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+                Find your inspiration
+              </h1>
+              <p className="mb-2">
+                Join the Photo-House community, home to tens of billions of{" "}
+                <br /> photos and 2 million groups.
+              </p>
+              <div>
+                <button type="button" className="btn-blue">
+                  Join Our Community
+                </button>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,11 @@
-// import required modules
-import { EffectFade, Autoplay } from "swiper";
 import type { NextPage } from "next";
-import Image from "next/image";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-import img from "../Images/landscape02.jpg";
-import img2 from "../Images/landscape03.jpg";
-import img3 from "../Images/landscape04.jpg";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-fade";
+import { Fragment } from "react";
+import Hero from "../components/home/Hero";
 
 const Home: NextPage = () => (
-  <div className="w-full min-h-[100vh]">
-    <Swiper
-      spaceBetween={30}
-      effect={"fade"}
-      autoplay={{
-        delay: 2000,
-        // crossFade: true,
-        disableOnInteraction: false,
-      }}
-      modules={[Autoplay, EffectFade]}
-      className="mySwiper"
-    >
-      <SwiperSlide>
-        <div className="h-[100vh] w-full">
-          <Image
-            src={img}
-            layout="fill"
-            objectFit="cover"
-            alt=""
-            quality={100}
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-[100vh] w-full ">
-          <Image
-            src={img2}
-            layout="fill"
-            objectFit="cover"
-            alt=""
-            quality={100}
-          />
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="h-[100vh] w-full ">
-          <Image
-            src={img3}
-            layout="fill"
-            objectFit="cover"
-            alt=""
-            quality={100}
-          />
-        </div>
-      </SwiperSlide>
-    </Swiper>
-  </div>
+  <Fragment>
+    <Hero />
+  </Fragment>
 );
 
 export default Home;
