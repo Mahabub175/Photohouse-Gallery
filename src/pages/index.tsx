3bc4640dfb3d35be3ae346cd7527f262eab1e1c7
Hasanul-Banna <hbdu006@gmail.com>
2022-11-18 18:49:53 +0600

    hero updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
+import axios from "axios";
 import type { NextPage } from "next";
-import Image from "next/image";
 import { Fragment } from "react";
 import Hero from "../components/home/Hero";
 import Magazines from "../components/home/Magazines";
 
-const Home: NextPage = () => (
+const Home: NextPage = ({ redirect_links, magazinesList }: any) => (
   <Fragment>
-    <Hero />
-    <Magazines />
+    <Hero RedirectLinks={redirect_links} />
+    <Magazines magazinesList={magazinesList} />
   </Fragment>
 );
 
 export default Home;
+export async function getStaticProps() {
+  const magazinesList = await axios.get('https://api.photohousemagazine.com/magazines').then((response) => {
+    return response.data.reverse()
+  }).catch((err) => console.log(err))
+  const redirect_links = await axios.get('https://api.photohousemagazine.com/redirect_links').then((response) => {
+    return response.data
+  }).catch((err) => console.log(err))
+  return { props: { magazinesList, redirect_links }, revalidate: 60 }
+}
