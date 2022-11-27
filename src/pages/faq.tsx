a4ebc265e3ae15bc4419b245e59f60408cc66999
Hasanul-Banna <hbdu006@gmail.com>
2022-11-27 18:38:33 +0600

    faq

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -20,8 +20,8 @@ const faq = () => {
                             <h1 className="mx-auto font-semibold text-lg text-white">1</h1>
                         </div>
                         <div className="order-1 bg-gradient-to-r from-gray-800 to-gray-600 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold  text-xl">Lorem Ipsum</h3>
-                            <p className="text-sm leading-snug tracking-wide  text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h3 className="mb-3 font-bold  text-xl text-emerald-300">Are submissions free here?</h3>
+                            <p className="text-sm leading-snug tracking-wide  text-opacity-100">There are two options available; free and paid options. You can submit photos for free here. On the other hand, the paid option will ensure our premium service to get you the highest exposure as a photographer.</p>
                         </div>
                     </div>
 
@@ -32,8 +32,8 @@ const faq = () => {
                             <h1 className="mx-auto text-white font-semibold text-lg">2</h1>
                         </div>
                         <div className="order-1 bg-gradient-to-r from-gray-600 to-gray-800 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
-                            <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h3 className="mb-3 font-bold text-xl text-emerald-300">How many photos can I submit here?</h3>
+                            <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">For a single magazine, you have to submit at least 4 photographs up to 12 maximums.</p>
                         </div>
                     </div>
 
@@ -44,13 +44,13 @@ const faq = () => {
                             <h1 className="mx-auto font-semibold text-lg text-white">3</h1>
                         </div>
                         <div className="order-1 bg-gradient-to-r from-gray-800 to-gray-600 rounded-lg shadow-xl w-5/12 px-6 py-4">
-                            <h3 className="mb-3 font-bold  text-xl">Lorem Ipsum</h3>
-                            <p className="text-sm leading-snug tracking-wide  text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h3 className="mb-3 font-bold  text-xl text-emerald-300">What genres of photographs can I submit for the magazine?</h3>
+                            <p className="text-sm leading-snug tracking-wide  text-opacity-100">#Beauty #Fashion #Men #Fine Art #Glamour #Nature #Travel #Wedding #Portrait #Fantasy #Photojournalism #Fitness #Hair #Kids.</p>
                         </div>
                     </div>
 
                     {/* <!-- left timeline --> */}
-                    <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+                    {/* <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
                         <div className="order-1 w-5/12"></div>
                         <div className="z-20 ring-2 ring-emerald-300 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
                             <h1 className="mx-auto text-white font-semibold text-lg">4</h1>
@@ -59,9 +59,10 @@ const faq = () => {
                             <h3 className="mb-3 font-bold text-white text-xl">Lorem Ipsum</h3>
                             <p className="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
+            {/* <p>For any types of questions or queries Photohouse official mail and contacts are always open for you!!</p> */}
         </div>
 
     );
