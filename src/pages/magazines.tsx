cbc7b3fdbd8263928e393e7dd7b640b930260bae
Hasanul-Banna <hbdu006@gmail.com>
2023-02-10 15:42:54 +0600

    swiper remove

--- src/pages/magazines.tsx
+++ src/pages/magazines.tsx
@@ -35,7 +35,7 @@ const Magazines: NextPage = ({ magazinesList }: any) => {
             {type}</span>
           )}
         </p> */}
-      <div className="container m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+      <div className="container m-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         {FilteredData.map((x: any) => (
           <div className="max-w-[280px] my-3 m-auto animate-fade" key={x._id}>
             <a target={"_blank"} href={x.redirect_link} rel="noreferrer" className="relative">
@@ -43,7 +43,7 @@ const Magazines: NextPage = ({ magazinesList }: any) => {
                 priority
                 src={x.image}
                 width={400}
-                height={510}
+                height={517}
                 layout="responsive"
                 alt="Magazines image"
                 className={`cursor-pointer rounded-md `}
