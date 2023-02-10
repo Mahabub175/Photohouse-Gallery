3d3c62e0bec7b3b56408afe00950c496359aaa8d
Hasanul-Banna <hbdu006@gmail.com>
2023-02-10 17:26:20 +0600

    news slide on maga

--- src/components/home/Magazines.tsx
+++ src/components/home/Magazines.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Image from "next/image";
 import { FC, useEffect, useState } from "react";
+import magaSliderStyles from "./magaSlide.module.css"
 
 const Magazines: FC = () => {
   const [magazinesList, setmagazinesList]: any[] = useState([])
@@ -12,13 +13,25 @@ const Magazines: FC = () => {
     }
     getData()
   }, [])
+  {/* <div className=" my-10 flex flex-row gap-3 overflow-x-auto scrollbar-hide">
+  {magazinesList.map((maga: any) => <img
+      src={maga?.image}
+      alt="img"
+      className="max-w-[25%] max-h-[45vh] rounded-md "
+      key={maga?._id} />)} 
+      </div>*/}
   return (
-    <div className=" my-10 flex flex-row gap-3 overflow-x-auto scrollbar-hide">
-      {magazinesList.map((maga: any) => <img
-        src={maga?.image}
-        alt="img"
-        className="max-w-[25%] max-h-[45vh] rounded-md "
-        key={maga?._id} />)}
+    <div className={magaSliderStyles.slider}>
+      <div className={magaSliderStyles["slide-track"]}>
+        {magazinesList.map((maga: any) => <div className={magaSliderStyles.slide} key={maga?._id}>
+          <img
+            src={maga?.image}
+            alt="img"
+            // className="max-w-[25%] max-h-[45vh] rounded-md "
+            className="h-[258px] w-[200px] rounded-sm"
+          />
+        </div>)}
+      </div>
     </div>
   );
 };
