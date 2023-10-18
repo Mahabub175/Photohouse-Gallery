97e7f038b76493a928ffb993e700069e3fe32ed8
Astro <rgrxmahabub.official@gmail.com>
2023-10-18 11:13:52 +0600

    updated

--- src/pages/magazines.tsx
+++ src/pages/magazines.tsx
@@ -24,14 +24,14 @@ const Magazines: NextPage = ({ magazinesList }: any) => {
   };
   return (
     <main className="py-4">
-      <div className="flex justify-between container m-auto items-center border-b-gray-500 border-b-2 pb-2 flex-col">
+      <div className="flex container justify-between m-auto items-center border-b-gray-500 border-b-2 pb-2 flex-col">
         <div className="w-full flex flex-col items-center ">
           <h1 className=" text-white sm:text-5xl text-3xl tracking-wider">
             Magazines
           </h1>
         </div>
       </div>
-      <div className="container m-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mt-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 mt-6 gap-x-8 container mx-auto mb-0 md:mb-24">
         {FilteredData.map((x: any) => (
           <div className="my-3 m-auto animate-fade" key={x._id}>
             <a
@@ -40,20 +40,20 @@ const Magazines: NextPage = ({ magazinesList }: any) => {
               rel="noreferrer"
               className="relative"
             >
-              <Image
+              {/* <Image
                 priority
                 src={base_url + "/" + x.thumbnail}
                 width={380}
-                height={550}
+                height={510}
                 layout="responsive"
                 alt="Magazines image"
                 className={`cursor-pointer rounded-md `}
-              />
-              {/* <img
+              /> */}
+              <img
                 src={base_url + "/" + x.thumbnail}
                 alt="Magazines image"
                 className="cursor-pointer rounded-md"
-              /> */}
+              />
             </a>
             <p className="text-white text-xs text-center">{x.name}</p>
           </div>
