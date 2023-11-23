25b8c2a54728cfa4b26fda5330de0dfd7c2af62d
Astro <rgrxmahabub.official@gmail.com>
2023-11-24 03:13:20 +0600

    updated

--- src/components/Contact/StaticInfo.tsx
+++ src/components/Contact/StaticInfo.tsx
@@ -33,7 +33,7 @@ export const StaticInfo = () => {
           </a>
         </li>
       </ul>
-      <div className="flex flex-wrap leading-[25px] absolute bottom-6 right-28">
+      <div className="flex flex-wrap leading-[25px] absolute bottom-6 right-28 md:left-10">
         <a
           href="https://www.facebook.com/photohouse.magazine"
           target="_blank"
